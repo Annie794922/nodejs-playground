6e4f2d6c6739d04a7e132d76be19509f005d9969
Annie Chang <yuchang370@gmail.com>
2022-05-11 11:42:31 +0800

    補充: 寫模組的順序

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 // console.log('Hello world!');
 
-const hello = require('./hello'); //引入hello.js這支檔案
+// 第一個區塊 內建模租
+
+const path = require('path'); //引入hello.js這支檔案
+const http = require('http');
+
+// 第二個區塊 第三方模組(套件)
+
+// 第三個區塊 自建模組
+const hello = require('./hello');
+
+////////////////////////////////////////////////////////////////
+
 
 hello.sayHello(); //在hello這支檔案執行say這個方法
 
