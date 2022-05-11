7ffc0db23b2e0e9dd62492f83bb918277c1773a3
Annie Chang <yuchang370@gmail.com>
2022-05-11 16:35:28 +0800

    3-1 使用 Express 創建 Web Server 並設定路由

--- app.js
+++ app.js
@@ -1,35 +1,47 @@
 // 第一個區塊 內建模租
 
-const http = require('http');
+// const url = require('url');
+const path = require('path');
 
 // 第二個區塊 第三方模組(套件)
+const express = require('express');
 
 // 第三個區塊 自建模組
 
 
 ////////////////////////////////////////////////////////////
 
-const server = http.createServer((req, res) => {
-	// console.log('第一個參數是瀏覽器對 web server 的 request', req);
-	// console.log('第二個參數是 web 要response 給瀏覽器的內容', res);
-    console.log('req url:', req.url);
+const app = express();
 
-    // if (req.url === '/login') {
-    //     res.stautsCode = 200; //server回應給瀏覽器的結果
-    //     return res.end('This is home page');
-    // } 
+// middleware(介於客戶端和瀏覽器之間的處理，決定要以什麼方式進行溝通--ex. 處理授權)
+app.use((req, res, next) => {
+    console.log('Hello!');
+    next(); //若沒加next，程式會不知道何時該結束，加了之後middleware才會以use.use.get的順序執行
+})
+app.use((req, res, next) => {
+    console.log('World!');
+    next();
+})
 
-    if (req.url === '/login') {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        return res.end('<h1>This is home page</h1>'); //return 一旦處理好就不再執行
-    } 
+// app.get('/', (req, res) => { //處理路由(原生寫法)，有人請求get的時候要做的事情(若沒有這段瀏覽器會不知道如何處理get)
+//     res.writeHead(200, { 'Content-Type': 'text/html' });
+//     res.write('<head><meta charset="utf-8" /></head>')
+//     res.write('<body>')
+//     res.write('<h1>這是首頁</h1>')
+//     res.write('</body>')
+// });
 
-    // if (req.url === '/login') {
-    //     return res.end('This is home page');
-    // }
-	// res.end(); //告訴程式讀取結束
+app.get('/', (req, res) => {
+    // res.writeHead(200, { 'Content-Type': 'text/html' });
+    // res.write('<head><meta charset="utf-8" /></head>')
+    // res.write('<body>')
+    // res.write('<h1>這是首頁</h1>')
+
+    //express寫法(status(200)讀取成功，並直接將檔案送出給瀏覽器)
+    res.status(200)
+        .sendFile(path.join(__dirname, 'views', 'index.html')); //(專案資料夾, 被讀取檔案所在的資料夾, 被讀取的檔案)
 });
 
-server.listen(3000, () => {
-	console.log('running server on port 3000');
+app.listen(3000, () => {
+	console.log('Web Server is running on port 3000');
 });
