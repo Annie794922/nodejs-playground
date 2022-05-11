563fb6e1dc912fafc7849b79979e8c8fe6c2b8b4
Annie Chang <yuchang370@gmail.com>
2022-05-11 15:53:06 +0800

    2-1 建立 Server 並監聽變化

--- app.js
+++ app.js
@@ -1,22 +1,35 @@
-// console.log('Hello world!');
-
 // 第一個區塊 內建模租
 
-const path = require('path'); //引入hello.js這支檔案
 const http = require('http');
 
 // 第二個區塊 第三方模組(套件)
 
 // 第三個區塊 自建模組
-const hello = require('./hello');
-
-////////////////////////////////////////////////////////////////
 
 
-const cowsay = require('cowsay');
+////////////////////////////////////////////////////////////
 
-console.log(cowsay.say({
-    text : "I'm a moooodule",
-    e : "oO",
-    T : "U "
-}));
+const server = http.createServer((req, res) => {
+	// console.log('第一個參數是瀏覽器對 web server 的 request', req);
+	// console.log('第二個參數是 web 要response 給瀏覽器的內容', res);
+    console.log('req url:', req.url);
+
+    // if (req.url === '/login') {
+    //     res.stautsCode = 200; //server回應給瀏覽器的結果
+    //     return res.end('This is home page');
+    // } 
+
+    if (req.url === '/login') {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        return res.end('<h1>This is home page</h1>'); //return 一旦處理好就不再執行
+    } 
+
+    // if (req.url === '/login') {
+    //     return res.end('This is home page');
+    // }
+	// res.end(); //告訴程式讀取結束
+});
+
+server.listen(3000, () => {
+	console.log('running server on port 3000');
+});
