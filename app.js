a12f17deadbd54c94aa6340aa9342d21d618a3c9
Annie Chang <yuchang370@gmail.com>
2022-05-12 14:50:50 +0800

    4-5 EJS include 使用模板片段

--- app.js
+++ app.js
@@ -61,7 +61,8 @@ app.post('/login', (req, res) => {
 
 app.get('*', (req, res) => {
     res.status(404)
-        .render('404');
+        .render('404',  {
+            pageTitle: 'Page not found'});
         // .sendFile(path.join(__dirname, 'views', '404.html'));
 }); //*萬用路由的位置要放在所有路由的最後面，否則會讓所有的路由皆顯示404
 
